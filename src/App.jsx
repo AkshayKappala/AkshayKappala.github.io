88624fba43ddacaf88292e6222017ba680025542
Akshay Kappala <akshaykappala98@gmail.com>
2025-11-18 19:30:50 -0600

    feat: add initial project structure with components, styles, and assets

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+	return (
+		<div className="container mx-auto max-w-7xl">
+			{/* navbar */}
+			<Navbar />
+			{/* hero */}
+			{/* about */}
+			{/* projects */}
+			{/* experience */}
+			{/* testimonials */}
+			{/* contact */}
+			{/* footer */}
+		</div>
+	);
+};
+
+export default App;
