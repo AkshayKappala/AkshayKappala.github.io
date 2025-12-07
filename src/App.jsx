c384aa9c5c429bc06a9da3aa56a42edc7ddf1286
Akshay Kappala <akshaykappala98@gmail.com>
2025-12-07 17:09:46 -0600

    feat: add Footer section with social links and legal information

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Experiences from "./components/Experiences";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -14,7 +15,7 @@ const App = () => {
 			<Projects />
 			<Experiences />
 			<Contact />
-			{/* footer */}
+			<Footer />
 		</div>
 	);
 };
